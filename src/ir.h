72f734d08070b4e4d773066db7c57efc58bec77c
kcomas <kevin.comas.git@gmail.com>
2021-01-27 18:32:48 -0500

    ir with sep types

--- src/ir.h
+++ src/ir.h
@@ -1,2 +1,29 @@
 
 #pragma once
+
+#include "ast.h"
+
+#define IR_PFX(NAME) OP_TYPE_##NAME
+
+typedef enum {
+    IR_PFX(_START_IR),
+    // DATA
+    IR_PFX(PUSH),
+    IR_PFX(ALLOCATE_LOCAL),
+    IR_PFX(FREE_LOCAL),
+    // OP
+    IR_PFX(_END_IR)
+} ir_op;
+
+typedef struct _ir ir;
+
+typedef union {
+
+} ir_data;
+
+typedef struct _ir {
+    ir_op op;
+    var_type_header type_header;
+    ir_data data;
+    ast_node *node;
+} ir;
